540d5e9a52b8cbe453a06cd5f55d7ee4df5e5cc6
binaymishramca@gmail.com <binaymishramca@gmail.com>
2017-09-02 21:56:32 +0530

    Security and log level configuration.

---
+++ src/main/java/jwt/spring/security/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package jwt.spring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
