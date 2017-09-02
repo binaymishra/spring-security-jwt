3638a6d0c2122d55e3c2fcffcaaac7f48183f6f9
binaymishramca@gmail.com <binaymishramca@gmail.com>
2017-09-02 22:15:33 +0530

    Basic endpoints

--- src/main/java/jwt/spring/security/Application.java
+++ src/main/java/jwt/spring/security/Application.java
@@ -2,10 +2,18 @@ package jwt.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @SpringBootApplication
 public class Application {
+
+	@GetMapping
+	void home(){}
+
+	@GetMapping("/api")
+	void api(){ }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
