60f4c8e56112a9fe7da512d710e2ef4138c2d62d
binaymishramca@gmail.com <binaymishramca@gmail.com>
2017-09-04 21:36:40 +0530

    /token endpoint for grtting a token

--- src/main/java/jwt/spring/security/Application.java
+++ src/main/java/jwt/spring/security/Application.java
@@ -1,9 +1,16 @@
 package jwt.spring.security;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * @author Binay Mishra
@@ -13,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
+	@Value("${jwt.secreteKey}")
+	private String secreteKey;
+
 	@GetMapping
 	String home(){
 		return "/";
@@ -21,6 +31,16 @@ public class Application {
 	@GetMapping("/api")
 	String api(){
 		return "/api";
+	}
+
+	@GetMapping("/token")
+	String token(){
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("username", "user");
+		claims.put("roles",    "USER, ADMIN");
+		return "Bearer "+Jwts.builder().signWith(SignatureAlgorithm.HS256, secreteKey)
+				.setClaims(claims)
+				.compact();
 	}
 
 	public static void main(String[] args) {
